40ce728587ffa98c284a341e44ba84f0a3c40048
Roshan Chilap <roshanchilap143@gmail.com>
2021-08-16 11:13:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(function timer() {
+  document.querySelector(".happy").innerHTML = "10";
+  setTimeout(function timer() {
+    document.querySelector(".happy").innerHTML = "9";
+    setTimeout(function timer() {
+      document.querySelector(".happy").innerHTML = "8";
+      setTimeout(function timer() {
+        document.querySelector(".happy").innerHTML = "7";
+        setTimeout(function timer() {
+          document.querySelector(".happy").innerHTML = "6";
+          setTimeout(function timer() {
+            document.querySelector(".happy").innerHTML = "5";
+            setTimeout(function timer() {
+              document.querySelector(".happy").innerHTML = "4";
+              setTimeout(function timer() {
+                document.querySelector(".happy").innerHTML = "3";
+                setTimeout(function timer() {
+                  document.querySelector(".happy").innerHTML = "2";
+                  setTimeout(function timer() {
+                    document.querySelector(".happy").innerHTML = "1";
+                    setTimeout(function timer() {
+                      document.querySelector(".happy").innerHTML =
+                        "Happy New Year !!!";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
